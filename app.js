06b84eaa682acc7766426204b6ceb76a59030c6e
veresrobert <veresrobert20@gmail.com>
2023-07-19 20:08:58 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Tema 01-----------------
+const myRecipe = {
+    title: "Pizza",
+    servings: 3,
+    ingredients: ["ham", "curd", "ketchup"]
+}
+
+
+
+console.log("Title: " + myRecipe.title)
+console.log("Servings: " + myRecipe.servings)
+console.log("Ingredients: " + myRecipe.ingredients)
+
+// Tema 02-------------------
+
+const obiect = {
+    a: 1,
+    b: 2,
+}
+
+delete obiect["b"]
+
+console.log(obiect)
+
+// Tema 03---------------------
+
+
