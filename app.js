365b6f7d6d1ca807ad83e88e6afdcaec27c03905
veresrobert <veresrobert20@gmail.com>
2023-07-21 13:51:58 +0300

    Commit nou

--- app.js
+++ app.js
@@ -1,27 +1,69 @@
-// Tema 01-----------------
-const myRecipe = {
-    title: "Pizza",
-    servings: 3,
-    ingredients: ["ham", "curd", "ketchup"]
+// Exercitiul 01-----------------
+let CreateRecipeObj = (title, servings, ingredients,) => {
+    let myRecipe = {
+        title: title,
+        servings: servings,
+        ingredients: ingredients 
+    };
+    console.log(myRecipe)
 }
 
+let title = "Pizza";
+let servings = 3;
+let ingredients = ["ham", "crud", "ketchup"];
 
 
-console.log("Title: " + myRecipe.title)
-console.log("Servings: " + myRecipe.servings)
-console.log("Ingredients: " + myRecipe.ingredients)
 
-// Tema 02-------------------
+CreateRecipeObj(title, servings, ingredients)
 
-const obiect = {
-    a: 1,
-    b: 2,
+// Exercitiul 02-------------------
+
+const object = (obj, proprietate) => {
+    let newObj = { ...obj};
+    delete newObj[proprietate];
+    console.log(newObj);    
 }
 
-delete obiect["b"]
+object({a: 1, b: 2}, 'b')
 
-console.log(obiect)
 
-// Tema 03---------------------
 
+// Exercitiul 03---------------------
+
+let books = [
+    {
+        titlu: "carte 1",
+        autor: "autor 1",
+        esteCitita: true,
+    },
+    {
+        titlu: "carte 1",
+        autor: "autor 1",
+        esteCitita: false,
+    },    
+    {
+        titlu: "carte 1",
+        autor: "autor 1",
+        esteCitita: true,
+    },
+];
+
+let displayBooks = (books) => {
+    for (n = 0; n < books.length; n++ ) {
+        // console.log(books[n])
+
+        let book = books[n];
+        let {titlu, autor, esteCitita} = book;
+        console.log(`${titlu} de ${autor} `);
+
+        if (esteCitita) {
+            console.log(`Ai citit deja "${titlu}" de ${autor}`)
+        } else {
+            console.log(`Trebuie sa citesti "${titlu}" de ${autor}`)
+        }
+
+    }
+} 
+
+displayBooks(books)
 
